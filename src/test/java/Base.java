5f2b08bf959f794240217450029b9d295063ee72
WardenRoad325 <Boyko02091998@gmail.com>
2023-11-26 00:38:20 +0300

    исправил тесты для регистрации, дрова не смог одолеть, перепробовал 10500 дров, не получается запустить с яндекса

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -10,6 +10,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class Base {
